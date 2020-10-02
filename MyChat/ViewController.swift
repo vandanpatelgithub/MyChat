1415bd758b2fbe66d24d19e9f88c58f34f580782
Vandan Patel <vandan.ets@gmail.com>
2020-10-02 17:00:19 -0400

    Initial Commit

---
+++ MyChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyChat
+//
+//  Created by Vandan Patel on 10/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

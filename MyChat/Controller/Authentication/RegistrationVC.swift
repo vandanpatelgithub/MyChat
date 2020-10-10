1bb299829f030be26e027496f4d39244908757e3
Vandan Patel <vandan.ets@gmail.com>
2020-10-10 14:34:50 -0400

    Implements Sign Up screen UI.

--- MyChat/Controller/Authentication/RegistrationVC.swift
+++ MyChat/Controller/Authentication/RegistrationVC.swift
@@ -11,12 +11,103 @@ class RegistrationVC: UIViewController {
     
     // MARK: - Properties
     
+    private let plusPhotoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "plus_photo"), for: .normal)
+        button.tintColor = .white
+        button.addTarget(self, action: #selector(handleSelectPhoto), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var emailContainerView: UIView = {
+        return InputContainerView(image: UIImage(systemName: "envelope") ?? UIImage(), textField: emailTextField)
+    }()
+    
+    private lazy var fullNameContainerView: UIView = {
+        return InputContainerView(image: #imageLiteral(resourceName: "person_outline"), textField: fullNameTextField)
+    }()
+    
+    private lazy var usernameContainerView: UIView = {
+        return InputContainerView(image: #imageLiteral(resourceName: "person_outline"), textField: usernameTextField)
+    }()
+    
+    private lazy var passwordContainerView: UIView = {
+        return InputContainerView(image: UIImage(systemName: "lock") ?? UIImage(), textField: passwordTextField)
+    }()
+    
+    private let signupButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign Up", for: .normal)
+        button.layer.cornerRadius = 5.0
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18.0)
+        button.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+        button.setTitleColor(.white, for: .normal)
+        button.setHeight(height: 50.0)
+        return button
+    }()
+    
+    private let alreadyHaveAnAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        let attributedTitle = NSMutableAttributedString(string: "Already have an account?  ",
+                                                        attributes: [.font: UIFont.systemFont(ofSize: 16),
+                                                                     .foregroundColor: UIColor.white])
+        attributedTitle.append(NSAttributedString(string: "Log In",
+                                                  attributes: [.font: UIFont.boldSystemFont(ofSize: 16),
+                                                               .foregroundColor: UIColor.white]))
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        button.addTarget(self, action: #selector(handleShowLogIn), for: .touchUpInside)
+        return button
+    }()
+    
+    private let emailTextField = CustomTextField(placeholder: "Email")
+    private let fullNameTextField = CustomTextField(placeholder: "Full Name")
+    private let usernameTextField = CustomTextField(placeholder: "Username")
+    private let passwordTextField: CustomTextField = {
+        let textField = CustomTextField(placeholder: "Password")
+        textField.isSecureTextEntry = true
+        return textField
+    }()
+    
     // MARK: - Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPurple
+        configureUI()
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func handleSelectPhoto() {
+        print("Select photo here...")
+    }
+    
+    @objc func handleShowLogIn() {
+        navigationController?.popViewController(animated: true)
     }
     
     // MARK: - Helpers
+    
+    func configureUI() {
+        configureGradientLayer()
+        
+        view.addSubview(plusPhotoButton)
+        plusPhotoButton.centerX(inView: view)
+        plusPhotoButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+        plusPhotoButton.setDimensions(height: 120, width: 120)
+        
+        let stack = UIStackView(arrangedSubviews: [emailContainerView,
+                                                   fullNameContainerView,
+                                                   usernameContainerView,
+                                                   passwordContainerView,
+                                                   signupButton])
+        stack.axis = .vertical
+        stack.spacing = 16.0
+        
+        view.addSubview(stack)
+        stack.anchor(top: plusPhotoButton.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 32, paddingLeft: 32, paddingRight: 32)
+        
+        view.addSubview(alreadyHaveAnAccountButton)
+        alreadyHaveAnAccountButton.centerX(inView: view)
+        alreadyHaveAnAccountButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor)
+    }
 }
